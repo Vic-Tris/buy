a383d2658ce2562c0ef116260171d347970c7879
Vic-Tris <y2jvictorious12@gmail.com>
2026-05-12 02:09:16 +0100

    initial commit

--- index.jsx
+++ index.jsx
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 import { createRoot} from " react-dom/client"
 
 //1. create root
 const root = createRoot()
+=======
+import { createRoot} from " react-dom/client"
+
+//1. create root
+const root = createRoot()
+>>>>>>> 2105962 (initial commit)
 //2. Render some markup to the root
